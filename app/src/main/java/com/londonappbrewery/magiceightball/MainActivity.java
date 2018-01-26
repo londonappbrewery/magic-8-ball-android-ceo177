eafb73e8aed9f4969e9b17af37bc41a5853547d5
ceo177 <34103930+ceo177@users.noreply.github.com>
2018-01-26 19:20:12 +0200

    Gradle Plugin & Build Tool Update

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        final ImageView imgBall = findViewById(R.id.image_eightBall);
+        Button btnAsk = findViewById(R.id.askButton);
+
+        final int[] ballAray = {R.drawable.ball1,
+        R.drawable.ball2,
+        R.drawable.ball3,
+        R.drawable.ball4,
+        R.drawable.ball5};
+
+
+        btnAsk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumerGenerator = new Random();
+                int number = randomNumerGenerator.nextInt(5);
+
+                imgBall.setImageResource(ballAray[number]);
+
+                Log.d("8ball", "number" + number);
+
+            }
+        });
+
+
+
     }
 }
